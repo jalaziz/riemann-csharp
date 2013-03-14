15c2a2a54bde95b6b4918a49d6bec78746ad7f04
Jameel Al-Aziz <jameel@scopely.com>
2013-03-14 07:57:32 +0000

    Background and metrics client (WIP)

---
+++ BackgroundBatchClient.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Riemann.Proto;
+
+namespace Riemann
+{
+    public class BackgroundBatchClient : BatchClient
+    {
+        public static readonly int DefaultFlushInterval = 5;
+
+        public int FlushInterval { get; set; }
+
+        protected CancellationTokenSource TokenSource { get; set; }
+        protected Task BackgroundTask { get; set; }
+
+        public BackgroundBatchClient(string host, int port, int? bufferSize = null, int? flushInterval = null)
+            : this(new Client(host, port), bufferSize, flushInterval)
+        { }
+
+        public BackgroundBatchClient(Client client, int? bufferSize = null, int? flushInterval = null)
+            : base(client, bufferSize ?? DefaultBatchSize)
+        {
+            FlushInterval = flushInterval ?? DefaultFlushInterval;
+
+            TokenSource = new CancellationTokenSource();
+
+            Start();
+        }
+
+        public void Start()
+        {
+            Stop();
+
+            var token = TokenSource.Token;
+            BackgroundTask = Task.Factory.StartNew(() => FlushTask(token), token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        public void Stop()
+        {
+            if (BackgroundTask != null)
+            {
+                TokenSource.Cancel();
+                try
+                {
+                    BackgroundTask.Wait(TimeSpan.FromSeconds(FlushInterval + 1));
+                }
+                catch (AggregateException)
+                {
+                    // ignore it, probably due to canceling
+                }
+            }
+        }
+
+        public override void SendEvent(Event ev)
+        {
+            EventQueue.Enqueue(ev);
+        }
+
+        public override void SendEvents(IEnumerable<Event> events)
+        {
+            foreach (var ev in events)
+            {
+                EventQueue.Enqueue(ev);
+            }
+        }
+
+        protected void FlushTask(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Flush();
+
+                if (!token.IsCancellationRequested)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(FlushInterval));
+                }
+            }
+
+            Flush();  // Flush before exiting
+            token.ThrowIfCancellationRequested();
+        }
+    }
+}
---
+++ Metrics.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Riemann.Proto;
+using Attribute = Riemann.Proto.Attribute;
+
+namespace Riemann
+{
+    public class Metrics
+    {
+        public string MachineName { get; set; }
+        public IBufferedClient RiemannClient { get; set; }
+
+        public Metrics(string host, int port, bool background = true)
+            : this(host, port, BatchClient.DefaultBatchSize, background)
+        { }
+
+        public Metrics(string host, int port, int batchSize, bool background = true)
+        {
+            MachineName = Utility.GetHostName();
+
+            if (background)
+            {
+                RiemannClient = new BackgroundBatchClient(host, port, batchSize);
+            }
+            else
+            {
+                RiemannClient = new BatchClient(host, port, batchSize);
+            }
+        }
+
+        protected internal void Event(string service, long value, string metric, IEnumerable<string> tags,
+                                      IEnumerable<Attribute> attributes)
+        {
+            var unixTime =
+                Convert.ToInt64((DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds);
+            var ev = new Event
+                {
+                    Host = MachineName,
+                    Service = service,
+                    MetricSint64 = value,
+                    Time = unixTime,
+                };
+            ev.Tags.Add(metric);
+
+            if (tags != null)
+            {
+                ev.Tags.AddRange(tags);
+            }
+
+            if (attributes != null)
+            {
+                ev.Attributes.AddRange(attributes);
+            }
+            
+            RiemannClient.SendEvent(ev);
+        }
+
+        public void Gauge(string service, long value, IEnumerable<string> tags = null,
+                          IEnumerable<Attribute> attributes = null)
+        {
+            Event(service, value, "gauge", tags, attributes);
+        }
+
+        public void Counter(string service, long value = 1, IEnumerable<string> tags = null,
+                            IEnumerable<Attribute> attributes = null)
+        {
+            Event(service, value, "counter", tags, attributes);
+        }
+
+        public void Meter(string service, IEnumerable<string> tags = null,
+                          IEnumerable<Attribute> attributes = null)
+        {
+            Event(service, 1, "meter", tags, attributes);
+        }
+
+        public void Histogram(string service, long value, IEnumerable<string> tags = null,
+                              IEnumerable<Attribute> attributes = null)
+        {
+            Event(service, value, "histogram", tags, attributes);
+        }
+
+        public Timer Timer(string service, bool autoStart = true, IEnumerable<string> tags = null,
+                           IEnumerable<Attribute> attributes = null)
+        {
+            return new Timer(this, service, autoStart, tags, attributes);
+        }
+
+        public void Flush()
+        {
+            RiemannClient.Flush();
+        }
+    }
+
+    public class Timer : IDisposable
+    {
+        protected Metrics Metrics { get; set; }
+        protected string Service { get; set; }
+        protected Stopwatch Stopwatch { get; set; }
+        protected IEnumerable<string> Tags { get; set; }
+        protected IEnumerable<Attribute> Attributes { get; set; } 
+
+        public Timer(Metrics metrics, string service, bool autoStart = true, IEnumerable<string> tags = null,
+                     IEnumerable<Attribute> attributes = null)
+        {
+            Metrics = metrics;
+            Service = service;
+            Stopwatch = new Stopwatch();
+            Tags = tags;
+            Attributes = attributes;
+
+            if (autoStart)
+            {
+                Start();
+            }
+        }
+
+        public void Reset()
+        {
+            Stopwatch.Reset();
+        }
+
+        public void Start()
+        {
+            Stopwatch.Start();
+            Metrics.Meter(Service, Tags, Attributes);
+        }
+
+        public void Stop()
+        {
+            Stopwatch.Stop();
+            Metrics.Event(Service, Stopwatch.ElapsedMilliseconds, "timer", Tags, Attributes);
+            Stopwatch.Reset();
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
